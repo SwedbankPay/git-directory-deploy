7952335b16049f24b4aa04bb3075aa1ed7ba3cfb
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-18 15:35:31 +0200

    Ignore SC1090

--- deploy.sh
+++ deploy.sh
@@ -39,6 +39,7 @@ parse_args() {
 
 	# Set args from file specified on the command-line.
 	if [[ $1 = "-c" || $1 = "--config-file" ]]; then
+		# shellcheck disable=SC1090
 		source "$2"
 		shift 2
 	fi
